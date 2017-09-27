6642086809c8d95091ca0b0e09d3edfc0af0bfed
Nosleepowl <844910815@qq.com>
2017-09-27 13:39:16 +0800

    更改文件的筛选
    希望更改文件的筛选方式 , 可以在选择文件的时候,看到文件里面的内容 , 最终没有实现 , 可以进行filter 的配置 , 只能在选择文件的时候进行展示

--- js/main-process/dialogs/open-file.js
+++ js/main-process/dialogs/open-file.js
@@ -5,7 +5,9 @@ const dialog = require('electron').dialog
 
 ipc.on('open-file-dialog', function (event) {
   dialog.showOpenDialog({
-    properties: ['openFile', 'openDirectory']
+    // 这里, 如果 同时设置了, openfile 和opeinDirectory 只有后者起作用
+    properties: ['openFile', 'openDirectory'],
+    filters: [{ extensions: ['xlsx', 'xls'], name: 'Excel' }]
   }, function (files) {
     if (files) event.sender.send('selected-directory', files)
   })
--- js/test.js
+++
@@ -1,6 +0,0 @@
-// C:\Users\uigsw\Desktop
-const path = require('path');
-const gold = require('glob');
-const  uuidv = require('uuid/v1');
-
-console.log(uuidv())
